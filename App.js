4a3693f39560a54f3e406f58007a12357dba26e0
Arvind <arvindcool2022@gmail.com>
2023-08-26 15:48:17 +0530

    ch - 04 assignment part 1

--- App.js
+++ App.js
@@ -16,9 +16,9 @@ header - sticky or fixed
 
 section
     -div
-        - 2 card carosel
+        - 2 card carousel
     -div
-        - dish type carosel
+        - dish type carousel
     -div
         - filters (if nav-bar crosses here it's contents changes to filter bar with small search)
     - container
@@ -86,9 +86,139 @@ const Header = () => {
   );
 };
 
+const CarouselSlide = () => {};
+const SmallCarouselItem = () => {};
+
+const Carousel = () => (
+  <div className="carousel">
+    <h1 className="carousel-title">Best offers for you</h1>
+    <div className="carousel__img-container">
+      <img
+        className="carousel__img"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/rng/md/carousel/production/d51fc17ab1049acb3b4cda413f86fad5"
+      />
+    </div>
+    <div className="carousel__img-container">
+      <img
+        className="carousel__img"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/rng/md/carousel/production/28f92512e727f99bbb1c90d2f469b2de"
+      />
+    </div>
+  </div>
+);
+
+const SmallCarousel = () => (
+  <div className="sm-carousel">
+    <h1 className="sm-carousel-title">What's in your mind?</h1>
+    <div className="sm-carousel__img-container">
+      <img
+        className="sm-carousel__img"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1675667625/PC_Creative%20refresh/Biryani_2.png"
+        alt="food"
+      />
+    </div>
+    <div className="sm-carousel__img-container">
+      <img
+        className="sm-carousel__img"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1675667626/PC_Creative%20refresh/South_Indian_4.png"
+        alt="food"
+      />
+    </div>
+    <div className="sm-carousel__img-container">
+      <img
+        className="sm-carousel__img"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029858/PC_Creative%20refresh/3D_bau/banners_new/Pure_Veg.png"
+        alt="food"
+      />
+    </div>
+    <div className="sm-carousel__img-container">
+      <img
+        className="sm-carousel__img"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029856/PC_Creative%20refresh/3D_bau/banners_new/Pizza.png"
+        alt="food"
+      />
+    </div>
+    <div className="sm-carousel__img-container">
+      <img
+        className="sm-carousel__img"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029845/PC_Creative%20refresh/3D_bau/banners_new/Burger.png"
+        alt="food"
+      />
+    </div>
+    <div className="sm-carousel__img-container">
+      <img
+        className="sm-carousel__img"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1675667625/PC_Creative%20refresh/North_Indian_4.png"
+        alt="food"
+      />
+    </div>
+  </div>
+);
+
+const Filters = () => (
+  <div>
+    <h1 className="filter-title">
+      Restaurants with online food delivery in Chennai
+    </h1>
+    <div
+      className="filters"
+      style={{ backgroundColor: 'red', height: '2rem' }}
+    ></div>
+  </div>
+);
+const Card = () => (
+  <div className="card">
+    <div className="card__img-container">
+      <img
+        className="card__img"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/zoyngbzc8cj9cromm3yr"
+        alt="food"
+      />
+      <span>15% off, upto Rs.200</span>
+    </div>
+    <div className="card__body">
+      <h3 className="card__title">A2B Adyar Ananda Bhavan</h3>
+      <p className="card__detail">South Indian, North Indian, Sweets</p>
+      <p className="card__rating">
+        4.8<i className="bi bi-star-fill"></i>
+        <span className="card__votes">(255)</span>
+      </p>
+    </div>
+  </div>
+);
+
+const CardContainer = () => (
+  <div className="container">
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+);
+const MainSection = () => (
+  <section className="main-section">
+    <Carousel />
+    <SmallCarousel />
+    <Filters />
+    <CardContainer />
+  </section>
+);
+
+const Footer = () => {};
+
 const AppLayout = () => (
   <div id="layout">
     <Header />
+    <MainSection />
+    <Footer />
   </div>
 );
 
