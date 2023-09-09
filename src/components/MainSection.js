11cdc1eb0eeb1785cc6919a5f8c2e7b608cecab2
Arvind <arvindcool2022@gmail.com>
2023-09-09 06:26:50 +0530

    fixed a morning bug

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -15,49 +15,67 @@ const MainSection = () => {
   const [smallCarouselList, setSmallCarouselList] = useState([]);
   const [resList, setResList] = useState([]);
   const [filterList, setFilterList] = useState([]);
+  console.log(carouselList, smallCarouselList, resList, ' rendered');
+
+  const fallBack = () => {
+    setCarouselList(CAROUSEL_DATA);
+    setSmallCarouselList(SMALL_CAROUSEL_DATA);
+    setResList(RESTAURANT_DATA);
+    setFilterList(RESTAURANT_DATA);
+  };
+
+  const fetchSwiggyData = async () => {
+    try {
+      const URL =
+        'https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0826802&lng=80.2707184&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
+      const data = await fetch(URL).catch(e =>
+        console.error('Not Fetched : ', e)
+      );
+      if (!data.ok) {
+        throw new Error(`Network response was not ok: ${data.status}`);
+      }
+      const json = await data.json();
+
+      setCarouselList(
+        json?.data?.cards[0]?.card?.card?.gridElements?.infoWithStyle?.info
+      );
+
+      setSmallCarouselList(
+        json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.info
+      );
+
+      setResList(
+        json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
+
+      setFilterList(
+        json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
+
+      console.log(
+        json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants,
+        ' use effect'
+      );
+    } catch (error) {
+      console.log(
+        "%cIMPORTANT: Unable to fetch live data from Swiggy due to a CORS issue. To see live data, please install a 'CORS-Allow' extension in your browser.",
+        'color:red;font-family:system-ui;font-size:2rem;font-weight:bold'
+      );
+      console.error('Error fetching data:', error);
+      fallBack();
+    }
+  };
 
   useEffect(() => {
-    (async function () {
-      try {
-        const URL =
-          'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.8438835&lng=80.05973639999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
-        const data = await fetch(URL).catch(e =>
-          console.error('Not Fetched : ', e)
-        );
-        if (!data.ok) {
-          throw new Error(`Network response was not ok: ${data.status}`);
-        }
-        const json = await data.json();
-
-        setCarouselList(
-          json.data.cards[0].card.card.gridElements.infoWithStyle.info
-        );
-
-        setSmallCarouselList(
-          json.data.cards[1].card.card.gridElements.infoWithStyle.info
-        );
-
-        setResList(
-          json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
-        );
-        setFilterList(
-          json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
-        );
-      } catch (error) {
-        console.log(
-          "%cIMPORTANT: Unable to fetch live data from Swiggy due to a CORS issue. To see live data, please install a 'CORS-Allow' extension in your browser.",
-          'color:red;font-family:system-ui;font-size:2rem;font-weight:bold'
-        );
-        console.error('Error fetching data:', error);
-        setCarouselList(CAROUSEL_DATA);
-        setSmallCarouselList(SMALL_CAROUSEL_DATA);
-        setResList(RESTAURANT_DATA);
-        setFilterList(RESTAURANT_DATA);
-      }
-    })();
+    fetchSwiggyData();
   }, []);
 
-  return (resList.length || smallCarouselList.length || carouselList.length) ===
+  return !(resList && smallCarouselList && carouselList) ? (
+    fallBack() //! if api returns undefined
+  ) : (resList.length && smallCarouselList.length && carouselList.length) ===
     0 ? (
     <h1>loading...</h1>
   ) : (
