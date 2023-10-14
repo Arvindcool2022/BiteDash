b97a3bca063fb355dfe7156efebf538e5de84c27
Arvind <arvindcool2022@gmail.com>
2023-10-14 18:23:40 +0530

    Testing setup.
    - installed react-testing-library.
    - installed jest
    - installed babel dependencies.
    - configured babel in `.babelrc`.
    - configured parcel to not use default parcel transplier.

--- src/components/MainSection/CarouselItem.js
+++ src/components/MainSection/CarouselItem.js
@@ -2,8 +2,10 @@ const CarouselItem = ({ object }) => (
   <div
     className="min-w-[420px]"
     onClick={() => (location.href = object.action.link)}
+    draggable="false"
   >
     <img
+      draggable="false"
       className="w-full object-cover cursor-pointer"
       src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/${object.imageId}`}
       alt="food"
--- src/components/MainSection/SmallCarouselItem.js
+++ src/components/MainSection/SmallCarouselItem.js
@@ -2,8 +2,10 @@ const SmallCarouselItem = ({ object }) => (
   <div
     className="min-w-[200px] aspect-square"
     onClick={() => (location.href = object.action.link)}
+    draggable="false"
   >
     <img
+      draggable="false"
       className="w-full h-full object-cover cursor-pointer"
       src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/${object.imageId}`}
       alt="food"
